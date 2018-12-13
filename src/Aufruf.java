5f99b07573481d31f40d4bc6eabd9fe4e05f5a70
TheresaSchmotz <schmotz.theresa@gmail.com>
2018-12-13 09:59:43 +0100

    Aufgabe 1 und 2

---
+++ src/Aufruf.java
@@ -0,0 +1,8 @@
+public class Aufruf {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
---
+++ src/at/ac/fh_kufstein/uebung_05/Classes/Fahrzeug.java
@@ -0,0 +1,15 @@
+package at.ac.fh_kufstein.uebung_05.Classes;
+
+public class Fahrzeug {
+
+    private short reifen;
+    private String farbe;
+    private short ps;
+    private short tueren;
+    private boolean gestartet;
+    private short geschwindigkeit;
+    public static int anzahl;
+
+
+
+}
